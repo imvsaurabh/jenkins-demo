674110a490fc478e10ec2d3ea35a03dd388776c5
sachinofficial71@gmail.com <sachinofficial71@gmail.com>
2024-08-07 00:36:18 +0530

    first commit

---
+++ src/test/java/com/github/imvsaurabh/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.github.imvsaurabh.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		JenkinsDemoApplication.main(new String[]{});
+	}
+
+}
